01d5c0f0404d491e60701627175bc0e0b08a756c
AndressaLAlcantara <andressalima8282@gmail.com>
2026-03-22 20:19:45 -0300

    Lista 1 de Exercícios de SO

---
+++ ESTSEQ.CT01_19.py
@@ -0,0 +1,15 @@
+#Declaração de Variáveis
+
+v1: int = 0
+v2: int = 0
+
+#Início
+
+v1 = int(input("Digite o primeiro valor real: "))
+v2 = int(input("Digite o segundo valor real: "))
+if v1>v2:
+    print("O maior valor real é: ", v1)
+else:
+    print("O maior valor real é: ", v2)
+
+#Fim
