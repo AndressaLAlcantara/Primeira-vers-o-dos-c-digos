abdf7e209df6b67a2e1e4c08d902c101814f7673
AndressaLAlcantara <andressalima8282@gmail.com>
2026-03-23 14:09:40 -0300

    Add files via upload

---
+++ ESTSEQ.CT01_44.py
@@ -0,0 +1,13 @@
+#Declaração de Variáveis
+
+b: int = 0
+e: int = 0
+p: int = 0
+
+#Início
+b = int(input("Digite o valor da base: "))
+e = int(input("Digite o valor do expoente: "))
+p = b**e
+print("O valor da potência é: ", p)
+
+#Fim
