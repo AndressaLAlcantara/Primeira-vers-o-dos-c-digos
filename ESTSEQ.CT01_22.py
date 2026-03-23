92c051b80146efcc24ac2af1febefe60a5b23359
AndressaLAlcantara <andressalima8282@gmail.com>
2026-03-22 21:00:30 -0300

    Add files via upload

---
+++ ESTSEQ.CT01_22.py
@@ -0,0 +1,13 @@
+#Declaração de Variáveis
+v1: int = 0
+v2: int = 0
+
+#Início
+v1 = int(input("Digite o primeiro valor: "))
+v2 = int(input("Digite o segundo valor: "))
+if v1>v2:
+    print("Os valores em ordem crecente: ", v2, v1)
+else:
+    print("Os valores em ordem crescente: ", v1, v2)
+
+#Fim
